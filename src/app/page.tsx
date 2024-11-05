10a3690accc3b428eee71a7659a2366e3d07de0b
itsabdullah2 <abdullahosman3020@gmail.com>
2024-11-05 20:34:45 +0200

    display content of the product page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import ProductItem from '../components/ProductItem';
+
 export default function Home() {
-  return <div className="">Some Shit</div>;
+  return (
+    <div className="flex-1 mt-20 max-w-[1440px] mx-auto px-5 xl:px-10">
+      <h1 className="text-2xl font-semibold">Products</h1>
+      <ProductItem />
+    </div>
+  );
 }
