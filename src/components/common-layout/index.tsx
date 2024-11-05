80d51b3aae2095f8ae6f97f00b160dc1949a9f92
itsabdullah2 <abdullahosman3020@gmail.com>
2024-11-05 20:40:19 +0200

    import Navbar & Footer component in CommonLayout

--- src/components/common-layout/index.tsx
+++ src/components/common-layout/index.tsx
@@ -2,13 +2,22 @@
 
 import Loading from '@/app/loading';
 import React, { Suspense } from 'react';
+import { Navbar, Footer } from '@/components';
 
 interface Props {
   children: React.ReactNode;
 }
 
 const CommonLayout = ({ children }: Props) => {
-  return <Suspense fallback={<Loading />}>{children}</Suspense>;
+  return (
+    <Suspense fallback={<Loading />}>
+      <div className="flex flex-col h-screen">
+        <Navbar />
+        {children}
+        <Footer />
+      </div>
+    </Suspense>
+  );
 };
 
 export default CommonLayout;
