fc8ccfee5317a14bbf6665820483ca5e56773816
itsabdullah2 <abdullahosman3020@gmail.com>
2024-11-05 20:39:26 +0200

    last v of this component 'all content displayed'

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex items-center justify-center py-5 px-5 xl:px-10 bg-primary text-white mt-5">
+      Made with LOvE By Abdullah Osman
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,26 @@
 import Link from 'next/link';
+import { IoMenu } from 'react-icons/io5';
+
+const liStyles =
+  'text-second hover:text-white font-medium text-lg duration-200';
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between p-5 bg-sky-500">
-      <Link href="/">Shoppy</Link>
-      <ul className="flex items-center gap-4 text-white">
-        <li>
-          <Link href="/products-page">Products</Link>
+    <nav className="flex items-center justify-between py-5 px-5 xl:px-10 bg-primary">
+      <Link href="/" className="text-white text-2xl font-bold">
+        Shoppy
+      </Link>
+      <ul className="hidden md:flex items-center gap-5">
+        <li className={`${liStyles}`}>
+          <Link href="/">Products</Link>
         </li>
-        <li>
-          <Link href="/cart-page">Cart</Link>
+        <li className={`${liStyles}`}>
+          <Link href="/cart">Cart</Link>
         </li>
       </ul>
+      <button className="text-second hover:text-white block md:hidden">
+        <IoMenu size={30} />
+      </button>
     </nav>
   );
 };
