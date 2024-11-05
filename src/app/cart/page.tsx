6dd1a476d74ec36f33e1d5c288c3be8834a04a9a
itsabdullah2 <abdullahosman3020@gmail.com>
2024-11-05 20:42:44 +0200

    display content of cart page

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,13 @@
+import CartItem from '@/components/CartItem';
+import Checkout from '@/components/Checkout';
+
+const CartPage = () => {
+  return (
+    <div className="flex-1 px-5 xl:px-10 mt-10 flex flex-col md:flex-row gap-5">
+      <CartItem />
+      <Checkout />
+    </div>
+  );
+};
+
+export default CartPage;
