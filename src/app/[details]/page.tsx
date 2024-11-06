1e7f87d11c7e6b0dc34dc39555d887a0a369f4cb
itsabdullah2 <abdullahosman3020@gmail.com>
2024-11-06 22:59:20 +0200

    display initial content

--- src/app/[details]/page.tsx
+++ src/app/[details]/page.tsx
@@ -1,5 +1,16 @@
-const DetailsPage = () => {
-  return <div>Details Page</div>;
-};
+'use client';
 
+import { ProductDetails } from '@/components';
+import { usePathname, useSearchParams } from 'next/navigation';
+
+const DetailsPage = () => {
+  const pathname = usePathname();
+  const id = pathname.split('/')[1];
+
+  return (
+    <div className="flex-1 mt-20 max-w-[1440px] mx-auto px-5 xl:px-10">
+      <ProductDetails productId={`${id}`} />
+    </div>
+  );
+};
 export default DetailsPage;
