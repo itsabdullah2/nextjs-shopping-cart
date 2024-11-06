2d1b06f58b98070b046fb169c800666ba35c3a14
itsabdullah2 <abdullahosman3020@gmail.com>
2024-11-06 22:55:13 +0200

    initial commit

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Checkout = () => {
-  return <div className="w-96 h-fit bg-slate-100 rounded-md p-5">Checkout</div>;
+  return (
+    <div className="w-96 h-fit bg-primary rounded-md p-5">
+      <h3 className="text-white leading-[150%] font-semibold text-xl border-b border-slate-500 pb-2">
+        Checkout
+      </h3>
+    </div>
+  );
 };
 
 export default Checkout;
