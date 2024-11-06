d4454e9be2fb4a3c45b2949657699d94a526df8e
itsabdullah2 <abdullahosman3020@gmail.com>
2024-11-06 22:54:36 +0200

    get the product that should to display on details page

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,9 +1,13 @@
 'use client';
 
 import { useAppState } from '@/context';
+import { listOfProducts } from '@/data';
 
-const ProductDetails = () => {
-  return <h1>Product Details</h1>;
+const ProductDetails = ({ productId }: { productId: string }) => {
+  const product = listOfProducts.find((product) => product.id === productId);
+
+  if (!product) return <h1>Product Not Found</h1>;
+
+  return <h1>{product.title}</h1>;
 };
-
 export default ProductDetails;
