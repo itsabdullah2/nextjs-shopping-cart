daed25b3904ebec6774765f371bc82df1d462f3c
itsabdullah2 <abdullahosman3020@gmail.com>
2024-11-06 22:57:45 +0200

    display ProductPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import ProductItem from '../components/ProductItem';
+import ProductsPage from './products/page';
 
 export default function Home() {
   return (
     <div className="flex-1 mt-20 max-w-[1440px] mx-auto px-5 xl:px-10">
       <h1 className="text-2xl font-semibold">Products</h1>
-      <ProductItem />
+      <ProductsPage />
     </div>
   );
 }
