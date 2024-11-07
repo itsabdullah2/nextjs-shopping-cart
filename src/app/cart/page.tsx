fb9485280036cceff16fc36101766eb5c9c9cd5f
itsabdullah2 <abdullahosman3020@gmail.com>
2024-11-07 18:07:15 +0200

    import CartItem & Checkout components and display them

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,13 +1,7 @@
-'use client';
-
 import CartItem from '@/components/CartItem';
 import Checkout from '@/components/Checkout';
-import { useAppState } from '@/context';
 
 const CartPage = () => {
-  const { cart } = useAppState() || {};
-  console.log(cart);
-
   return (
     <div className="flex-1 px-5 xl:px-10 mt-10 flex flex-col md:flex-row gap-5">
       <CartItem />
