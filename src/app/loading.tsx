ea8a1610fa34c60b624f225810404f9a07d5b3c2
itsabdullah2 <abdullahosman3020@gmail.com>
2024-11-07 18:03:46 +0200

    import the SkeletonCard component & handle the usage of it

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,14 @@
+import { SkeletonCard } from '@/components';
+
 const Loading = () => {
   return (
-    <div className="text-primary font-medium text-[18px]">Loading....</div>
+    <div className="mx-auto flex-1 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
+      {Array(12)
+        .fill(0)
+        .map((_, index) => (
+          <SkeletonCard key={index} />
+        ))}
+    </div>
   );
 };
 
