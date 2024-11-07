75a0bf4ad8023ceaedf4b7e2a8192726a9e73bbf
itsabdullah2 <abdullahosman3020@gmail.com>
2024-11-07 18:59:50 +0200

    display all the content of product details & all the functionality & responsiveness

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,6 +1,5 @@
-'use client';
-
-import { useAppState } from '@/context';
+import { AddToCartBtn } from './';
+import Image from 'next/image';
 import { listOfProducts } from '@/data';
 
 const ProductDetails = ({ productId }: { productId: string }) => {
@@ -8,6 +7,32 @@ const ProductDetails = ({ productId }: { productId: string }) => {
 
   if (!product) return <h1>Product Not Found</h1>;
 
-  return <h1>{product.title}</h1>;
+  return (
+    <div className="flex-1 grid grid-cols-1 lg:grid-cols-2 gap-10">
+      <Image
+        src={product.imgUrl}
+        alt={product.title}
+        width={500}
+        height={500}
+        className="bg-cover w-full h-full rounded-xl shadow-xl"
+        loading="lazy"
+      />
+      <div className="flex flex-col">
+        <h2 className="text-primary font-semibold mb-2 text-2xl capitalize">
+          {product.title}
+        </h2>
+        <p className="text-second font-medium leading-normal mb-3">
+          {product.description}
+        </p>
+        <span className="w-full md:w-[400px] border-b border-second border-[3px] rounded-full" />
+        <h3 className="text-[36px] md:text-[48px] font-bold text-primary mt-4">
+          <span className="text-3xl font-medium">Price:</span> ${product.price}
+        </h3>
+        <div className="mt-3">
+          <AddToCartBtn product={product} productId={product.id} />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default ProductDetails;
